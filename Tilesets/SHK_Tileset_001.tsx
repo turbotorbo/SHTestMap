f82e0830fe0147cd56c4bd286dc68a7d7f1f9b39
turbotorbo <vigoroso@live.de>
2021-12-08 14:12:21 +0100

    deleted tileset folder

--- Tilesets/SHK_Tileset_001.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="SHK_Tileset_001" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="../../../../Freelance_Projects/20_Schlachthof Kassel/SHK_TileMap_Test001.png" width="128" height="128"/>
-</tileset>
